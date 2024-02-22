a91c0eac8f16ceee1f0706207c38b4ac4254db9a
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-22 21:02:54 +0530

    created compiler for java, python and javascript, added contest page and problem solving page and code editor,

--- Server/index.js
+++ Server/index.js
@@ -1,39 +1,37 @@
 const express = require("express");
 require("dotenv").config();
-const {connection} = require("./config/db");
-const {userRouter} = require("./routes/user.route");
-const {questionRouter} = require("./routes/question.route");
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/user.route");
+const { questionRouter } = require("./routes/question.route");
 const { QuestionModel } = require("./models/question.model");
 const { auth } = require("./middleware/auth.middleware");
 const cors = require("cors");
+const { compileRouter } = require("./routes/compilerRouter");
 
-
-const app= express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/users" , userRouter);
-app.use("/question" , questionRouter);
-
+app.use("/users", userRouter);
+app.use("/questions", questionRouter);
+app.use("/compile", compileRouter);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to home page");
-  });
+  res.send("Welcome to home page");
+});
 
-  app.get("/questions",auth, async(req, res) => {
-    const questions = await QuestionModel.find();
-    res.status(200).send({questions})
- });
+app.get("/questions", auth, async (req, res) => {
+  const questions = await QuestionModel.find();
+  res.status(200).send({ questions });
+});
 
-
-app.listen(process.env.port ,async()=>{
-   try{
-     await connection;
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
     console.log(`Server is running at http://localhost:${process.env.port}`);
     console.log("codeFlow database is connected..");
-
-   }catch(err){
+  } catch (err) {
     console.log(err);
-   }
-})
+  }
+});
