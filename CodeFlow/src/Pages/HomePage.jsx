59ecd286a0e1470175794440778b055d22fa2cff
geeteshmehuria <geeteshmaihuria@gmail.com>
2024-02-23 00:15:11 +0530

    homepage

--- CodeFlow/src/Pages/HomePage.jsx
+++ CodeFlow/src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import heroImg from "../assets/heroImage2.png";
 import Footer from "../Components/Footer";
 import { Link } from "react-router-dom";
@@ -7,7 +7,11 @@ function HomePage() {
   const auth = false;
   return (
     <>
-      <Box className="bg-gradient-to-br from-slate-500 via-gray-700 to-zinc-800">
+      <Box
+        className="bg-gradient-to-br from-slate-500 via-gray-700 to-zinc-800"
+        pb={{ base: "", md: "2.3rem" }}
+        // clipPath="polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%)"
+      >
         <Box
           w={{ base: "100%", md: "90%" }}
           m="auto"
@@ -25,7 +29,7 @@ function HomePage() {
             mt={{ base: "2rem", md: "" }}
             // border={"1px solid white"}
           >
-            <Image src={heroImg} filter={"drop-shadow(0 0 0.75rem #854CE6)"} />
+            <Image src={heroImg} filter={"drop-shadow(0 0 0.75rem #90ee90)"} />
           </Box>
           <Box
             w={{ base: "95%", md: "50%" }}
@@ -38,7 +42,8 @@ function HomePage() {
               className="text-white"
               fontSize={{ base: "2.2rem", md: "2.9rem" }}
               w={{ base: "100%", md: "90%" }}
-              filter={"drop-shadow(0 0 0.45rem #854CE6)"}
+              // filter={"drop-shadow(0 0 0.35rem black)"}
+              textAlign={{ base: "justify", md: "left" }}
             >
               The best place to build, test, and discover front-end code.
             </Heading>
@@ -46,6 +51,7 @@ function HomePage() {
               className="text-white"
               fontSize={{ base: "1rem", md: "1.4rem" }}
               mt={"1rem"}
+              textAlign={"justify"}
             >
               CodePen is a social development environment for front-end
               designers and developers. Build and deploy a website, show off
@@ -59,13 +65,17 @@ function HomePage() {
                   <Button
                     mt={"2rem"}
                     fontSize={"1.3rem"}
-                    color={"black"}
-                    fontWeight={"bold"}
-                    bg={"#a576f5"}
+                    color={"white"}
+                    // #a576f5
+                    bg={"#134232"}
                     p={"1.3rem 3rem"}
-                    filter={"drop-shadow(0 0 0.45rem #854CE6)"}
-                    // variant={"outline"}
-                    _hover={{ bg: "#5629a3", color: "white" }}
+                    // filter={"drop-shadow(0 0 0.85rem #854CE6)"}
+                    // border={".2px solid white"}
+                    _hover={{
+                      bg: "#90ee90",
+                      color: "#134232",
+                      // border: ".2px solid #a576f5",
+                    }}
                   >
                     Dashboard
                   </Button>
@@ -77,20 +87,102 @@ function HomePage() {
                   <Button
                     mt={"2rem"}
                     fontSize={"1.3rem"}
-                    color={"#eaf0c9"}
-                    fontWeight={"bold"}
-                    bg={"#a576f5"}
+                    color={"white"}
+                    // #a576f5
+                    bg={"#134232"}
                     p={"1.3rem 3rem"}
-                    // filter={"drop-shadow(0 0 0.35rem #854CE6)"}
-                    // variant={"outline"}
-                    _hover={{ bg: "#5629a3", color: "white" }}
+                    // filter={"drop-shadow(0 0 0.85rem #854CE6)"}
+                    // border={".2px solid white"}
+                    _hover={{
+                      bg: "#90ee90",
+                      color: "#134232",
+                      // border: ".2px solid #a576f5",
+                    }}
                   >
-                    SingUp
+                    Sign Up
                   </Button>
                 </Link>
               </>
             )}
           </Box>
+        </Box>
+      </Box>
+      <Box
+        className="bg-gradient-to-tr from-gray-800 via-stone-600 to-stone-400"
+        // clipPath="polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%)"
+      >
+        <Box
+          w={{ base: "100%", md: "90%" }}
+          m="auto"
+          p={{ base: ".6rem 2rem", md: "3rem" }}
+          // border={"2px solid white"}
+          color={"white"}
+        >
+          <Flex
+            gap={{ base: "2rem", md: "3rem" }}
+            textAlign={"justify"}
+            flexDirection={{ base: "column", md: "row" }}
+            m={{ base: "2rem 0rem" }}
+          >
+            <Box
+              p={5}
+              border={"1px solid white"}
+              w={{ base: "100%", md: "90%" }}
+              borderRadius={"1rem"}
+            >
+              <Heading
+                textAlign={"center"}
+                mb={{ base: 2, md: 5 }}
+                // filter={"drop-shadow(0 0 0.35rem black)"}
+                // filter={"drop-shadow(0 0 0.25rem #90ee90)"}
+              >
+                Build & Test
+              </Heading>
+              <Text color={"#bba67d"}>
+                Get work done quicker by building out entire projects or
+                isolating code to test features and animations.{" "}
+              </Text>
+            </Box>
+            <Box
+              p={5}
+              border={"1px solid white"}
+              w={{ base: "100%", md: "90%" }}
+              borderRadius={"1rem"}
+            >
+              <Heading
+                textAlign={"center"}
+                mb={{ base: 2, md: 5 }}
+                // filter={"drop-shadow(0 0 0.35rem black)"}
+                // filter={"drop-shadow(0 0 0.25rem #90ee90)"}
+              >
+                Learn & Discover
+              </Heading>
+              <Text color={"#bba67d"}>
+                Want to upgrade your skills and get noticed? Participating in
+                CodePen Challenges is a great way to try something new.
+              </Text>
+            </Box>
+            <Box
+              p={5}
+              border={"1px solid white"}
+              borderRadius={"1rem"}
+              w={{ base: "100%", md: "90%" }}
+            >
+              <Heading
+                textAlign={"center"}
+                mb={{ base: 2, md: 5 }}
+                // filter={"drop-shadow(0 0 0.35rem black)"}
+                // filter={"drop-shadow(0 0 0.25rem #90ee90)"}
+              >
+                Share Your Work
+              </Heading>
+              <Text color={"#bba67d"}>
+                Become a part of the most active front-end community in the
+                world by sharing work. Presenting at a conference? Show your
+                code directly in the browser{" "}
+              </Text>
+            </Box>
+          </Flex>
         </Box>
       </Box>
       <Footer />
