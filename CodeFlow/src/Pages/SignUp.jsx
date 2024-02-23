595d7af4faeabd618757e49536bd8314dbf14760
Deepakkumarrp <deepakkumarrp@gmail.com>
2024-02-23 10:51:55 +0530

    LogIN Route Added

--- CodeFlow/src/Pages/SignUp.jsx
+++ CodeFlow/src/Pages/SignUp.jsx
@@ -164,7 +164,7 @@ function SignUP() {
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/login" variant="body2">
-                  Already have an account? Sign in
+                  Already have an account? Log in
                 </Link>
               </Grid>
             </Grid>
