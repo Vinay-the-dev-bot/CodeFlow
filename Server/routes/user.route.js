fada7d51cf413be9281a41d1919d5b9f9f256ae9
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-24 21:37:18 +0530

    added submission model, and submission feature

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -6,6 +6,8 @@ const { blackListTokenModel } = require("../models/blacklist.model");
 const { AnswerModel } = require("../models/answer.model");
 const { auth } = require("../middleware/auth.middleware");
 const { access } = require("../middleware/access.middleware");
+const { submissionModel } = require("../models/submission.Model");
+const { QuestionModel } = require("../models/question.model");
 
 const userRouter = express.Router();
 
@@ -48,7 +50,7 @@ userRouter.post("/login", async (req, res) => {
           const token = jwt.sign({ userID: user._id }, "codeflow", {
             expiresIn: "7d",
           });
-          res.status(200).send({ msg: "Login Successfull!", token });
+          res.status(200).send({ msg: "Login Successfull!", token, user });
         } else {
           res
             .status(200)
@@ -75,22 +77,63 @@ userRouter.get("/logout", async (req, res) => {
 
 // post question answer
 
+userRouter.post("/submission2", async (req, res) => {
+  const { questionID, userID, code, results } = req.body;
+  try {
+    if (!userID || !questionID) {
+      console.log("ANANAK");
+      return res
+        .status(400)
+        .send({ msg: "Bad Request. userID and questionID are required." });
+    }
+    const question = await QuestionModel.findOne({ _id: questionID });
+    const solPoints = question.points;
+    console.log(solPoints);
+
+    const user = await UserModel.findById(userID);
+    if (!user.solved_questions.includes(questionID)) {
+      await UserModel.findByIdAndUpdate(userID, {
+        $push: { solved_questions: questionID },
+        $inc: { points: solPoints },
+      });
+    }
+
+    const ans = new submissionModel({
+      questionID,
+      userID,
+      results,
+      code,
+    });
+    await ans.save();
+
+    res.status(200).send({ msg: "solution submitted." });
+  } catch (err) {
+    console.log("Error:", err);
+    res.status(500).send({ msg: "Internal Server Error." });
+  }
+});
+
 userRouter.post("/answer/:questionID", auth, async (req, res) => {
   const { questionID } = req.params;
   const { userID, answer } = req.body;
 
   try {
-
     if (!userID || !questionID) {
-      return res.status(400).send({ msg: "Bad Request. userID and questionID are required." });
+      return res
+        .status(400)
+        .send({ msg: "Bad Request. userID and questionID are required." });
     }
 
     const user = await UserModel.findById(userID);
     if (user.solved_questions.includes(questionID)) {
-      return res.status(400).send({ msg: "Bad Request. Question already solved by the user." });
+      return res
+        .status(400)
+        .send({ msg: "Bad Request. Question already solved by the user." });
     }
 
-    await UserModel.findByIdAndUpdate(userID, { $push: { solved_questions: questionID } });
+    await UserModel.findByIdAndUpdate(userID, {
+      $push: { solved_questions: questionID },
+    });
 
     const ans = new AnswerModel({ questionID, userID, answer });
     await ans.save();
@@ -104,17 +147,14 @@ userRouter.post("/answer/:questionID", auth, async (req, res) => {
 
 // get submissions
 
-userRouter.get("/submissions",auth,async(req,res)=>{
-  try{
-    
-      const answer =await AnswerModel.find({userID:req.body.userID})
-      res.status(200).send({answer})
-  }catch(err){
-      res.status(400).send({"error":err})
+userRouter.get("/submissions", auth, async (req, res) => {
+  try {
+    const answer = await AnswerModel.find({ userID: req.body.userID });
+    res.status(200).send({ answer });
+  } catch (err) {
+    res.status(400).send({ error: err });
   }
 });
-
-
 
 userRouter.get("/", auth, async (req, res) => {
   // console.log(req.body);
