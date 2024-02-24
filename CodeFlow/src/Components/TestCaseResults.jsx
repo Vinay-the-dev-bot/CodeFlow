bc9bd2258a4e990e063eeab20d123eca43084ac8
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-24 21:36:27 +0530

    made changes to incorporate results from eternal API results

--- CodeFlow/src/Components/TestCaseResults.jsx
+++ CodeFlow/src/Components/TestCaseResults.jsx
@@ -1,4 +1,6 @@
-const TestCaseResults = ({ results }) => {
+import axios from "axios";
+
+const TestCaseResults = ({ code, questionId, results }) => {
   // console.log("RESULT", results);
   // const testCasePasses = results.reduce((res) => {
   //    res.expe.replaceAll(" ", "") ==
@@ -6,14 +8,33 @@ const TestCaseResults = ({ results }) => {
   // }, pass);
 
   const pass = 0;
-  const testCasePasses = results.reduce((acc, res) => {
+  const testCasePassed = results.reduce((acc, res) => {
     return res.expe.replaceAll(" ", "") ===
       res.stdout.replaceAll("\r\n", "").replaceAll(" ", "")
       ? acc + 1
       : acc;
   }, pass);
 
-  console.log("Number of test cases passed:", testCasePasses);
+  console.log("Number of test cases passed:", testCasePassed);
+
+  const percPassed = (testCasePassed / results.length) * 100;
+
+  const saveResults = async () => {
+    const res = await fetch(`http://localhost:4500/users/submission2`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        questionID: questionId,
+        code,
+        results,
+        userID: "65d9cbc56c9b36cd3dd0bf62",
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
 
   // [
   //   {
@@ -35,7 +56,7 @@ const TestCaseResults = ({ results }) => {
   // ];
   return (
     <div className="testCaseResults w-4/5  my-10 m-auto border-2 py-10 h-fit ">
-      <div className="w-4/5 mx-auto my-2 text-center text-5xl ">Results</div>
+      {/* <div className="w-4/5 mx-auto my-2 text-center text-5xl ">Results</div> */}
       {/* <div>{JSON.stringify(question)}</div>
       <div>{JSON.stringify(results)}</div> */}
       {/* <div className="flex flex-col gap-5">
@@ -56,6 +77,21 @@ const TestCaseResults = ({ results }) => {
           })}
       </div> */}
       <div className="flex flex-col  rounded-md  gap-5">
+        <div className="flex items-center justify-around">
+          {
+            <p className="text-center  text-3xl p-5 ">
+              {JSON.stringify(percPassed)} % testcases passed
+            </p>
+          }
+          {percPassed == 100 && (
+            <button
+              className="px-5  rounded-xl h-fit p-1    bg-emerald-500 "
+              onClick={saveResults}
+            >
+              saveResults
+            </button>
+          )}
+        </div>
         <div
           className={`w-4/5 flex  items-center m-auto  rounded-md border-2 h-10  `}
         >
@@ -65,6 +101,7 @@ const TestCaseResults = ({ results }) => {
           <p>|</p>
           <p className="w-1/2 text-center  rounded-md m-auto">OutPut</p>
         </div>
+
         {results.map((res, i) => {
           return (
             <div
--- CodeFlow/src/Pages/solve.jsx
+++ CodeFlow/src/Pages/solve.jsx
@@ -24,18 +24,33 @@ function Solve() {
   return (
     <div>
       {/* <p>{JSON.stringify(question)}</p> */}
-      <div className="px-10  pt-10 flex flex-col gap-5">
-        <h1 className="text-2xl font-bold "> Statement</h1>
-        <p className="text-2xl ">
-          {question.title} |
-          <span className={`px-2 ${question.difficulty}`}>
+      <div className="flex p-10">
+        <div className="w-3/5 px-10    flex flex-col gap-5">
+          <h1 className="text-2xl font-bold "> Statement</h1>
+          <p className="text-2xl ">
+            {question.title}
+            {/* |
+            <span className={`px-2 ${question.difficulty}`}>
+              {question.difficulty}
+            </span> */}
+          </p>
+          <h1 className="text-2xl font-bold "> Description</h1>
+          <p>{question.description}</p>
+          <h1 className="text-2xl font-bold"> Constraints</h1>
+          <p>{question.constraints}</p>
+        </div>
+        <div className="w-1/5 mx-auto flex flex-col gap-5 justify-evenly">
+          <p
+            className={`p-2 text-2xl text-center w-4/5 ${question.difficulty}`}
+          >
             {question.difficulty}
-          </span>
-        </p>
-        <h1 className="text-2xl font-bold "> Description</h1>
-        <p>{question.description}</p>
-        <h1 className="text-2xl font-bold"> Constraints</h1>
-        <p>{question.constraints}</p>
+          </p>
+          <p
+            className={`p-2 w-4/5 text-2xl text-center rounded-md bg-green-600 points points-${question.points}`}
+          >
+            {question.points} Points
+          </p>
+        </div>
       </div>
       <QuestionJudge question={question} questionId={questionId} />
     </div>
