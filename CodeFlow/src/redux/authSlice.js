b7d30a0a72f416f57e8c920dcbd5801f550f4abe
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-12 20:10:45 +0530

    store updated

--- CodeFlow/src/redux/authSlice.js
+++ CodeFlow/src/redux/authSlice.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const name = localStorage.getItem("name");
 const token = localStorage.getItem("token");
 const user = JSON.parse(localStorage.getItem("user"));
-console.log("UUSERjdjdf", user);
 const initialState = { auth: token ? true : false, name: name, user };
 
 export const authSlice = createSlice({
